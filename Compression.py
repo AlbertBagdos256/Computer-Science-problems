7c11a652e037b705304fd79c74aa03a7d681beed
AlbertBagdos256 <44371952+AlbertBagdos256@users.noreply.github.com>
2019-11-23 21:40:31 +0500

    Update Compression.py

--- Compression.py
+++ Compression.py
@@ -1,48 +1,51 @@
 
-
 class CompressedGene:
     def __init__(self, gene: str) -> None:
         self._compress(gene)
+
     def _compress(self, gene: str) -> None:
-        self.bit_string: int = 1  # начальная метка
+        self.bit_string: int = 1  # start with sentinel
         for nucleotide in gene.upper():
-            self.bit_string <<= 2  # сдвиг влево на 2 бита
-            if nucleotide == "А":  # поменять 2 последних бита на 00
-                self.bit_string |= "0b00"
-            elif nucleotide == "С":  # поменять 2 последних бита на 01
-                self.bit_string |= "0b01"
-            elif nucleotide == "G":  # поменять 2 последних бита на 10
-                self.bit_string |= "0b0"
-            elif nucleotide == "Т":  # поменять 2 последних бита на 11
-                self.bit_string |= "0b1"
+            self.bit_string <<= 2  # shift left two bits
+            if nucleotide == "A":  # change last two bits to 00
+                self.bit_string |= 0b00
+            elif nucleotide == "C":  # change last two bits to 01
+                self.bit_string |= 0b01
+            elif nucleotide == "G":  # change last two bits to 10
+                self.bit_string |= 0b10
+            elif nucleotide == "T":  # change last two bits to 11
+                self.bit_string |= 0b11
             else:
-                raise ValueError("Invalid Nucleotide:{}".format(nucleotide)) 
-            
-                      
+                raise ValueError("Invalid Nucleotide:{}".format(nucleotide))
+
     def decompress(self) -> str:
         gene: str = ""
-        for i in range(0, self.bit_string.bit_length() - 1, 2):# -1 чтобы исключить метку
-            bits: int = self .bit_string >> i & "0b1" # получить только 2 значимых бита
-            if bits == "0b00":  # А
-                gene += "А"
-            elif bits == "0b01":  # с
-                gene += "С"
-            elif bits == "0b0":  # G
+        for i in range(0, self.bit_string.bit_length() - 1, 2):  # - 1 to exclude sentinel
+            bits: int = self.bit_string >> i & 0b11  # get just 2 relevant bits
+            if bits == 0b00:  # A
+                gene += "A"
+            elif bits == 0b01:  # C
+                gene += "C"
+            elif bits == 0b10:  # G
                 gene += "G"
-            elif bits == "0b1":  # т
-                gene += "Т"
+            elif bits == 0b11:  # T
+                gene += "T"
             else:
-                raise ValueError("Invalid bts:{}".format(bits))
-            return gene[::-1]  # [::-1] обращение строки посредством обратных срезов
-            
-            
-    def __str__(self) -> str:  # представление строки в биде красивого вывода
+                raise ValueError("Invalid bits:{}".format(bits))
+        return gene[::-1]  # [::-1] reverses string by slicing backwards
+
+    def __str__(self) -> str:  # string representation for pretty printing
         return self.decompress()
-        if __name__ == '__main__':
-            from sys import getsizeof
-            original: str ="TAGGGAТТAACCGТТATATATATATAGCCATGGATCGAТТATATAGGGAТТAACCGТТATATATATATAGCCATGGATCGAТТATA" * 100
-            print("original is {} bytes".format(getsizeof(original)))
-            compressed: CompressedGene = CompressedGene(original) # сжатие
-            print("compressed is {} bytes".format(getsizeof(compressed.bit_string)))
-            print(compressed)  # распаковка
-            print("original and decompressed are the same: {}".format(original == compressed.decompress()))
+
+
+if __name__ == "__main__":
+    from sys import getsizeof
+    original: str = "TAGGGATTAACCGTTATATATATATAGCCATGGATCGATTATATAGGGATTAACCGTTATATATATATAGCCATGGATCGATTATA" 
+    print("original is {} bytes".format(getsizeof(original)))
+    compressed: CompressedGene = CompressedGene(original)  # compress
+    print("compressed is {} bytes".format(getsizeof(compressed.bit_string)))
+    print(compressed)  # decompress
+    print("original and decompressed are the same: {}".format(original == compressed.decompress()))
+
+
+
